826a4a90943cdb5c0c4c66d905da9138b2f32a26
salman <70580281+salman9011@users.noreply.github.com>
2024-01-29 23:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,10 @@
 		document.cookie = `fontsize${fontsize};`
 		document.cookie = `fontcolor${fontcolor};`
 		document.documentElement.style.setProperty("--fontsize", `${fontsize??16}px`);
-document.documentElement.style.setProperty("--fontcolor", `${fontcolor??"000"}`);
+document.documentElement.style.setProperty("--fontcolor", `${fontcolor??"#000"}`);
 	})
 		let cookies =Object.fromEntries(document.cookie.split("; ").map((item)=>item.split("=")));
 document.getElementById("fontsize").value=cookies.fontsize??16;
 document.getElementById("fontcolor").value=cookies.fontcolor??"#000000";
 document.documentElement.style.setProperty("--fontsize", `${cookies.fontsize??16}px`);
-document.documentElement.style.setProperty("--fontcolor", `${cookies.fontcolor??"000"}`);
+document.documentElement.style.setProperty("--fontcolor", `${cookies.fontcolor??"#000"}`);
