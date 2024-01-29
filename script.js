f822be5bbeb7bc4403fee871e6470c2df368d40c
salman <70580281+salman9011@users.noreply.github.com>
2024-01-29 23:29:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,9 @@
 		document.documentElement.style.setProperty("--fontsize", `${fontsize??16}px`);
 document.documentElement.style.setProperty("--fontcolor", `${fontcolor??"#000"}`);
 	})
-		let cookies =Object.fromEntries(document.cookie.split("; ").map((item)=>item.split("=")));
+		let cookies =Object.fromEntries(
+			document.cookie.split("; ").
+			map((item)=>item.split("=")));
 document.getElementById("fontsize").value=cookies.fontsize??16;
 document.getElementById("fontcolor").value=cookies.fontcolor??"#000000";
 document.documentElement.style.setProperty("--fontsize", `${cookies.fontsize??16}px`);
